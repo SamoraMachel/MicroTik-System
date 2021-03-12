f5b9e306e7a196a48593d6270ff06808168ca721
Arnold Wamae <arnoldwamae2@gmail.com>
2021-03-12 23:00:48 +0300

    Css and Bootstrap Changes. Restructured routerLogin Blade

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,9 +15,10 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
-
+     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('styles')
+   
 </head>
 <body>
     <div id="app">
--- resources/views/microtik/login.blade.php
+++ resources/views/microtik/login.blade.php
@@ -1,25 +1,11 @@
 @extends('layouts.app')
 
-<!DOCTYPE html>
-<html lang="en">
+    @section('styles')
 
-<head>
+    <link href="/css/routerLogin.css" rel="stylesheet" media="all">
 
-    <!-- Icons font CSS-->
-    <link href="vendor/mdi-font/css/material-design-iconic-font.min.css" rel="stylesheet" media="all">
-    <link href="vendor/font-awesome-4.7/css/font-awesome.min.css" rel="stylesheet" media="all">
-    <!-- Font special for pages-->
-    <link href="https://fonts.googleapis.com/css?family=Roboto:100,100i,300,300i,400,400i,500,500i,700,700i,900,900i" rel="stylesheet">
+    @endsection
 
-    <!-- Vendor CSS-->
-    <link href="vendor/select2/select2.min.css" rel="stylesheet" media="all">
-    <link href="vendor/datepicker/daterangepicker.css" rel="stylesheet" media="all">
-
-    <!-- Main CSS-->
-    <link href="css/main.css" rel="stylesheet" media="all">
-</head>
-
-<body>
     @section('content')
 
     <div class="page-wrapper bg-red p-t-180 p-b-100 font-robo">
@@ -28,36 +14,49 @@
                 <div class="card-heading"></div>
                 <div class="card-body">
                     <h2 class="title">Router Login</h2>
-                    <form method="POST">
-                        
+                    <form action="{{route('router_verify')}}" method="POST">
+                        @csrf
                         <div class="row row-space">
-                            <div class="col-2">
+                            <div class="col-6">
                                 <div class="input-group">
                                     <input class="input--style-2" type="text" placeholder="Remote Router IP Address" name="ip">
+
+                                    @error('ip')
+                                        <span class="text-danger font-weight-bold">{{$message}}</span>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
 
                         <div class="row row-space">
-                            <div class="col-2">
+                            <div class="col-6">
                                 <div class="input-group">
                                     <input class="input--style-2" type="text" placeholder="Username" name="username">
+                                     @error('username')
+                                        <span class="text-danger font-weight-bold">{{$message}}</span>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
 
                         <div class="row row-space">
-                            <div class="col-2">
+                            <div class="col-6">
                                 <div class="input-group">
                                     <input class="input--style-2" type="password" placeholder="Password" name="password">
+                                    @error('password')
+                                        <span class="text-danger font-weight-bold">{{$message}}</span>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
 
                         <div class="row row-space">
-                            <div class="col-2">
+                            <div class="col-6">
                                 <div class="input-group">
                                     <input class="input--style-2" type="number" placeholder="Port" name="port">
+                                    @error('port')
+                                        <span class="text-danger font-weight-bold">{{$message}}</span>
+                                    @enderror
                                 </div>
                             </div>
                         </div>
@@ -73,7 +72,3 @@
 
     @endsection
 
-</body>
-
-</html>
-<!-- end document-->
